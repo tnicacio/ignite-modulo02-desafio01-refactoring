efe0d20d9e8f769a7c89f9a934bf14210a2d95db
tnicacio <nicacio.t92@gmail.com>
2021-03-20 22:37:57 -0300

    Modal Add Food refactored

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,7 +11,8 @@ import { Container } from './styles';
 
 interface InputProps {
   name: string;
-  icon: any;
+  icon?: any;
+  placeholder: string;
 }
 
 const Input = ({ name, icon: Icon, ...rest }:InputProps ) => {
