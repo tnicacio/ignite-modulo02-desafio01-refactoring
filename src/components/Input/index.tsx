6cc014db5c08e3344643d20a59be3328069c518a
tnicacio <nicacio.t92@gmail.com>
2021-03-20 22:01:05 -0300

    Input refactored

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon: any;
+}
+
+const Input = ({ name, icon: Icon, ...rest }:InputProps ) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -23,7 +28,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
